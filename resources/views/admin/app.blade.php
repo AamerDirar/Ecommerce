0125e5a777d2381e148d42e3dafbd9df38f7dc28
AamerIdris <aamer.idris@munjz.com>
2022-08-05 13:11:25 +0300

    replace laravel mix with vite

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -5,24 +5,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-        
+
         <link rel="icon" type="image/x-icon" href="{{ Storage::url('images/favicon.png') }}">
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/admin/app.css') }}">
-
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/admin/app.js') }}" defer></script>
-        @inertiaHead
+        @vite('resources/js/admin/app.js')
     </head>
     <body class="font-sans antialiased">
         @inertia
-
-        @env ('local')
-            <script src="http://localhost:8080/js/bundle.js"></script>
-        @endenv
     </body>
 </html>
